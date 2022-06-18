2787358832b41724fa16bb631fd0c25e356975cc
FUSIOZ031 <62032230+FUSIOZ031@users.noreply.github.com>
2022-06-18 10:51:14 +0700

    Update main.c

--- Lab3/main/main.c
+++ Lab3/main/main.c
@@ -1,68 +1,89 @@
-#include "driver/gpio.h"
-#include "esp_spi_flash.h"
-#include "esp_system.h"
+#include <stdio.h>
+#include <string.h>
+#include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_freertos_hooks.h"
-#include "sdkconfig.h"
-#include <stdio.h>
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+#include "freertos/queue.h"
 
-char task1Param[12] = "task1Param";
-char task2Param[12] = "task2Param";
-char task3Param[12] = "task3Param";
+#define NumberOfTask 3
+#define delay 1000
+#define wait_time 100
 
-void task1(void * parameter)
+typedef struct Data
 {
-    printf((char *)parameter);
-    // loop forever //
-    volatile uint32_t ul;
-    while (1)
+    int id;
+    int state;
+    int comingTask;
+} Data;
+
+static QueueHandle_t queue;
+
+void readyTask(void *pv)
+{
+    for (int i = 0; i <= 100; i++)
     {
-        for( ul = 0; ul < 50; ul++ )
-        {
-            printf("task1 is running\n");
-        }
-        vTaskDelay(500 / portTICK_PERIOD_MS);
-        printf("task1 is ending\n");
+        Data task;
+        task.id = i;
+        if (i % 2 == 0)
+		{
+            task.state = 1; 
+		}
+        else if (i % 3 == 0)
+		{
+			task.state = 2; 
+		}
+        else if (i % 7 == 0)
+		{
+			task.state = 3; 
+		}
+        else
+		{
+			task.state = -1;
+		}
+        task.comingTask = 0;
+        xQueueSend(queue, &task, wait_time);
+        vTaskDelay(delay / portTICK_RATE_MS);
     }
     vTaskDelete(NULL);
 }
 
-void task2(void * parameter)
+void runningTask(void *pv)
 {
-    printf((char *)parameter);
-    volatile uint32_t ul;
-    while (1)
+    Data task;
+    char *name_task;
+    name_task = (char *)pv;
+    for (;;)
     {
-        for( ul = 0; ul < 50; ul++ )
+        if (xQueueReceive(queue, (void *)&task, wait_time) == pdTRUE)
         {
-            printf("task2 is running\n");
+            if (((task.state == 1) && (!strcmp(name_task, "task1"))) || ((task.state == 2) && (!strcmp(name_task, "task2"))) || ((task.state == 3) && (!strcmp(name_task, "task3"))))
+                printf("%s comes and does the request id %d \n", name_task, task.id);
+            else
+            {
+                task.comingTask += 1;
+                if (NumberOfTask > task.comingTask)
+                    xQueueSendToFront(queue, &task, wait_time);
+                else
+                    printf("Warning: no task does the request id %d \n", task.id);
+            }
         }
-        vTaskDelay(500 / portTICK_PERIOD_MS);
-        printf("task2 is ending\n");
+        vTaskDelay(delay / portTICK_RATE_MS);
     }
     vTaskDelete(NULL);
 }
 
-void task3(void * parameter)
-{
-    printf((char *)parameter);
-    volatile uint32_t ul;
-    while (1)
-    {
-    vTaskDelay(3000 / portTICK_PERIOD_MS);
-    for( ul = 0; ul < 50; ul++ )
-    {
-        printf("task3 is running\n");
-    }
-    printf("task3 is ending\n");
-    }
-    vTaskDelete(NULL);
-    }
+static const char *task0 = "task0";
+static const char *task1 = "task1";
+static const char *task2 = "task2";
+static const char *task3 = "task3";
 
-void app_main()
+void app_main(void)
 {
-    xTaskCreate(task1, "task1", 10000, (void *)task1Param, 1, NULL);
-    xTaskCreate(task2, "task2", 10000, (void *)task2Param, 1, NULL);
-    xTaskCreate(task3, "task3", 10000, (void *)task3Param, 5, NULL);
+    queue = xQueueCreate(10, sizeof(Data));
+    xTaskCreatePinnedToCore(&readyTask, "Task 0", 2048, (void *)task0, 2, NULL, 1);
+    xTaskCreatePinnedToCore(&runningTask, "Task 1", 2048, (void *)task1, 1, NULL, 1);
+    xTaskCreatePinnedToCore(&runningTask, "Task 2", 2048, (void *)task2, 1, NULL, 1);
+    xTaskCreatePinnedToCore(&runningTask, "Task 3", 2048, (void *)task3, 1, NULL, 1);
 }
